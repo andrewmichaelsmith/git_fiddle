0e8dd8e2123f418d4067d0ca93f826e205e8af2d
Woop <woop@woop.com>
2014-02-15 12:35:05 +0000

    ok

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import tempfile
 import os
 import sys
 import tarfile
+import argparse
 
 from git import (
     Repo,
+    IndexEntry,
 )
 
 ROOT             = os.path.dirname(os.path.realpath(__file__))
@@ -15,7 +17,46 @@ def run():
 
     init()
 
+    file_name = get_file_name()
+
+    logging.info("Going to try and create a repo called: %s", file_name)
+
+    create_repo_with_file(file_name)
+
+def get_file_name():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file_name', help='file name to create')
+
+    args = parser.parse_args()
+
+    return args.file_name
+
+def create_repo_with_file(file_name):
     repo_dir = create_new_base_repo()
+
+    repo = Repo(repo_dir)
+
+    #UGLY BIT
+    #This is to get around the fact that index.add checks the file
+    #exists. Could probably get around this in a more intelligent
+    #way
+    fake_file_name = "a" * len(file_name)
+    open("%s/%s" % (repo_dir, fake_file_name), 'a').close()
+
+    repo.index.add([fake_file_name])
+
+    file_index = repo.index.entries[(fake_file_name, 0)]
+
+    new_file_index = IndexEntry((file_index[0], file_index[1], file_index[2], file_name))
+
+    repo.index.entries[(fake_file_name, 0)] = new_file_index
+    #/UGLY BIT
+
+
+    commit = repo.index.commit("my first commit")
+
+    repo.index.write_tree()
+
 
 def create_new_base_repo():
     base_tar = tarfile.open(BASE_REPO_TAR_GZ)
@@ -23,7 +64,7 @@ def create_new_base_repo():
 
     base_tar.extractall(path=tmp)
 
-    logging.info("Created base repo at %s", tmp)
+    logging.info("Created repo at %s", tmp)
 
     return tmp
 
