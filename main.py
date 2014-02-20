403c182728cd85ecef1b25fcd1c0f31a0f9f73af
Woop <woop@woop.com>
2014-02-20 22:30:57 +0000

    some stuff

--- main.py
+++ main.py
@@ -4,14 +4,16 @@ import os
 import sys
 import tarfile
 import argparse
-
-from git import (
-    Repo,
-    IndexEntry,
-)
+import hashlib
+import shutil
+import zlib
 
 ROOT             = os.path.dirname(os.path.realpath(__file__))
-BASE_REPO_TAR_GZ = "%s/%s" % (ROOT, "base-repo.tar.gz")
+
+BASE_REPO_TAR_GZ = "%s/%s" % (ROOT, "pre-made-repo.tar.gz")
+#This describes the contents of BASE_REPO_TAR_GZ
+TREE_HASH        = "775a99b79ec5fbdca77d58ca69db102ba9f42098"
+FILE_NAME        = "aaaaa"
 
 def run():
 
@@ -32,29 +34,33 @@ def get_file_name():
     return args.file_name
 
 def create_repo_with_file(file_name):
-    """
-        You want a file called "..", we make a git repo
-        with a file called "aa".
-
-        We then take the tree that points to "aa" and replace
-        it with ".."
-
-        This is quite a hacky way to do this.
-    """
     repo_dir = create_new_base_repo()
 
-    repo = Repo(repo_dir)
+    obj_dir = "%s/.git/objects" % repo_dir
 
-    fake_file_name = "a" * len(file_name)
-    open("%s/%s" % (repo_dir, fake_file_name), 'a').close()
+    tree_file = "%s/%s/%s" % (obj_dir, TREE_HASH[:2], TREE_HASH[2:])
 
-    repo.index.add([fake_file_name])
+    with open(tree_file, 'r+') as f:
+        raw = f.read()
 
-    file_index = repo.index.entries[(fake_file_name, 0)]
+        blob = zlib.decompress(raw)
+        blob = blob.replace(FILE_NAME, file_name)
 
-    commit = repo.index.commit("my first commit")
+        f.seek(0)
+        f.write(zlib.compress(blob))
+        f.truncate()
 
-    repo.index.write_tree()
+
+    new_hash = hashlib.sha1(blob).hexdigest()
+
+    new_tree_dir =  "%s/%s/" % (obj_dir, new_hash[:2])
+
+    if not os.path.exists(new_tree_dir):
+        os.makedirs(new_tree_dir)
+
+    new_tree_file = "%s/%s" % (new_tree_dir, new_hash[2:])
+
+    shutil.move(tree_file, new_tree_file)
 
 
 def create_new_base_repo():
