25402a243c093e886ada9e1d057a9b49a5c785bf
Woop <woop@woop.com>
2014-02-26 22:12:32 +0000

    re-jigging

---
+++ create_repo_main.py
@@ -0,0 +1,62 @@
+import logging
+import sys
+
+from git_obj import (
+    Blob,
+    Tree,
+    Commit,
+    Repo,
+)
+
+def run():
+    init()
+    #basic_repo()
+    #one_level_repo()
+    multiple_commits()
+
+
+def multiple_commits():
+
+    commit_1 = Commit(None, Tree([Blob(body="xx", name="HI")]))
+    commit_2 = Commit(commit_1, Tree([Blob(body="yy", name="HO")]))
+
+    repo = Repo()
+    repo.set_head(commit_2)
+
+    #Andy Todo
+
+    repo.write()
+
+def one_level_repo():
+
+    tree_a = Tree([Blob(body="xx", name="boo")], name="boogaloo")
+    tree_b = Tree([tree_a])
+
+    commit = Commit(None, tree_b)
+
+    repo = Repo()
+    repo.set_head(commit)
+    repo.write()
+
+def basic_repo():
+
+    blobs = []
+    for x in xrange(100):
+        blobs.append(Blob(body="hi", name="..%s" % x))
+
+    tree = Tree(blobs)
+    commit = Commit(None,tree)
+
+    repo = Repo()
+    repo.set_head(commit)
+    repo.write()
+
+
+
+def init():
+    logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
+
+if __name__ ==  "__main__":
+    run()
+
+
